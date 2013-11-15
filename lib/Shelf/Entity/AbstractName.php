7589df92901663e73d1096a6358ea64286738491
Russell Ahlstrom <russell.ahlstrom@gmail.com>
2013-11-15 16:02:58 -0700

    Adds getName to Collection Entity
    Adds a getName method to the Collection entity. This resulted in the
    Boardgame Name entity having some methods move out to an AbstractName
    entity which the Boardgame and Collection name entities then extend
    from.
    
    Adds tests for new classes and methods. Fixes an issue with getting
    the sort name when the sort_index is 1.

---
+++ lib/Shelf/Entity/AbstractName.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Shelf\Entity;
+
+use Shelf\Entity\AbstractDataEntity;
+
+/**
+ * Class used to represent a name of an entity
+ */
+abstract class AbstractName extends AbstractDataEntity
+{
+    /**
+     * Returns the sort index with an option to make it zero based
+     *
+     * @param boolean $zeroBased OPTIONAL
+     *
+     * @return int
+     */
+    public function getSortIndex($zeroBased = false)
+    {
+        $sortIndex = parent::getSortIndex();
+        if ($zeroBased) {
+            --$sortIndex;
+        }
+        return $sortIndex;
+    }
+
+    /**
+     * Returns the value starting with the sort index
+     *
+     * @param $appendPreSort OPTIONAL Controls whether the characters pre-sort
+     *                                index are appended to returned result
+     *
+     * @return string
+     */
+    public function getSortValue($appendPreSort = true)
+    {
+        $value = $this->getValue();
+        $sortIndex = $this->getSortIndex(true);
+
+        $sortValue = substr($value, $sortIndex);
+        if ($appendPreSort && $sortIndex > 0) {
+            $sortValue .= ', ' . substr($value, 0, $sortIndex - 1);
+        }
+        return $sortValue;
+    }
+}
---
+++ lib/Shelf/Entity/Collection/Name.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Shelf\Entity\Collection;
+
+use Shelf\Entity\AbstractName;
+
+/**
+ * Class used to represent the name of a board game and the various properties
+ * associated with that name
+ */
+class Name extends AbstractName
+{
+}
