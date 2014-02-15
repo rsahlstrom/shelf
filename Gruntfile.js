572adee4bdcd89f8c9dcc51c46cbc787ad2e690a
Russell Ahlstrom <russell.ahlstrom@gmail.com>
2014-02-15 16:21:59 -0700

    Adds Grunt and Changes to PSR-4
    Adds gruntfile to run phpcs, phplint, phpunit, and jsonlint.
    
    Changes composer to user PSR-4 to flatten out the structure a little
    bit.

---
+++ Gruntfile.js
@@ -0,0 +1,71 @@
+/* globals module */
+/* globals require */
+
+module.exports = function (grunt) {
+    require('time-grunt')(grunt);
+    require('load-grunt-tasks')(grunt);
+
+    var directoriesConfig = {
+        composer: 'vendor',
+        bin: 'bin',
+        lib: 'lib',
+        tests: 'tests'
+    };
+
+    grunt.initConfig({
+        directories: directoriesConfig,
+        githooks: {
+            all: {
+                'pre-commit': 'test'
+            }
+        },
+        phplint: {
+            options: {
+                swapPath: '/tmp'
+            },
+            all: [
+                '<%= directories.lib %>/**/*.php'
+            ]
+        },
+        phpcs: {
+            src: {
+                dir: '<%= directories.lib %>'
+            },
+            options: {
+                bin: '<%= directories.bin %>/phpcs',
+                standard: 'psr2',
+                extensions: 'php'
+            }
+        },
+        phpunit: {
+            classes: {
+                dir: '<%= directories.tests %>'
+            },
+            options: {
+                bin: '<%= directories.bin %>/phpunit',
+                bootstrap: '<%= directories.tests %>/bootstrap.php',
+                colors: true
+            }
+        },
+        jsonlint: {
+            all: {
+                src: [
+                    '*.json',
+                    '<%= directories.lib %>/Config/**/*.json'
+                ]
+            }
+        }
+    });
+
+    grunt.registerTask('default', [
+        'githooks',
+        'test'
+    ]);
+
+    grunt.registerTask('test', [
+        'phplint',
+        'phpcs',
+        'phpunit',
+        'jsonlint'
+    ]);
+};
