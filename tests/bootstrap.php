0ec6d814911bb7deb1633267e8f38f19ac8ab365
Russell Ahlstrom <russell.ahlstrom@gmail.com>
2014-02-15 17:56:50 -0700

    Adds PSR-4 Autoloading to the Tests
    Flattens the test directory by switching from psr-0 to psr-4 for the
    unit tests. Removes the manual adding of the name space in the
    bootstrap.php file.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,5 @@
 <?php
 
-define('FIXTURE_DIR', __DIR__ . '/Shelf/Test/Fixture');
+define('FIXTURE_DIR', __DIR__ . '/Fixture');
 
 $loader = require(__DIR__ . '/../vendor/autoload.php');
-$loader->add('Shelf\Test', __DIR__);
